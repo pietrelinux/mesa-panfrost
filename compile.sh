8eff8ba882d642d06ddd8cc43fae1f13cecefce1
pietre.linux <pietre.linux@gmail.com>
2019-12-29 19:02:58 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -10,6 +10,5 @@ sudo -E ninja -C build install
 cd ..
 git clone git://anongit.freedesktop.org/mesa/mesa
 cd mesa
-meson -Ddri-drivers= -Dvulkan-drivers= -Dgallium-drivers=panfrost,kmsro -Dlibunwind=false -Dprefix=/usr build/
-ninja -C build/
+meson -Ddri-drivers= -Dvulkan-drivers= -Dgallium-drivers=panfrost,kmsro -Dlibunwind=false -Dplatforms=x11,drm,wayland,surfaceless -Dprefix=/usr build/ninja -C build/
 sudo ninja -C build/ install
